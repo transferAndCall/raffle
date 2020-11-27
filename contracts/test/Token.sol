2d772827283db9356fb951a16e324ecea4fd126d
transferAndCall <transferAndCall@protonmail.com>
2020-11-27 16:24:33 -0600

    Give name and symbol to Token

--- contracts/test/Token.sol
+++ contracts/test/Token.sol
@@ -3,7 +3,13 @@ pragma solidity ^0.6.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
-  constructor() ERC20("Token", "LPT") public {
+  constructor(
+    string memory _name,
+    string memory _symbol
+  )
+    ERC20(_name, _symbol)
+    public
+  {
     _mint(msg.sender, 1000 * 1e18);
   }
 }
